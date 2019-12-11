809e7648b411a34dce8524e0f49cdc3abe97ab1b
Кочкин Александр <Alexander.Kochkin@artezio.com>
2019-12-11 16:59:28 +0400

    Added ancestors and descendants, controlled and uncontrolled forms.

---
+++ src/shared/components/pitch/index.js
@@ -0,0 +1,124 @@
+import React from 'react';
+
+import './index.css';
+
+class Pitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      role: '',
+      nameValid: false,
+      ageValid: false,
+      roleValid: false,
+      roles: ['', 'Гость', 'Модератор', 'Администратор']
+    }
+    this.nameOe = React.createRef();
+    this.ageOe = React.createRef();
+    this.roleOe = React.createRef();
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onAgeChange = this.onAgeChange.bind(this);
+    this.onRoleChange = this.onRoleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  validatorRequired(field) {
+    return !!field;
+  }
+
+  validateName(login) {
+    return this.validatorRequired(login);
+  }
+
+  validateAge(age) {
+    return this.validatorRequired(age);
+  }
+
+  validateRole(role) {
+    return this.validatorRequired(role);
+  }
+
+  onNameChange() {
+    const value = this.nameOe.current.value;
+    const valid = this.validateName(value);
+    this.setState({name: value, nameValid: valid});
+  }
+
+  onAgeChange() {
+    const value = this.ageOe.current.value;
+    const valid = this.validateAge(value);
+    this.setState({age: value, ageValid: valid});
+  }
+
+  onRoleChange() {
+    const value = this.roleOe.current.value;
+    const valid = this.validateRole(value);
+    this.setState({role: value, roleValid: valid});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.nameValid && this.state.ageValid && this.state.roleValid) {
+      console.log('Отправлено. Форма с неуправляемым компонентом.');
+      console.log({
+        name: this.state.name,
+        age: this.state.age,
+        role: this.state.role
+      });
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>pitch works!</p>
+        <h3>Форма с неуправляемым компонентом</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="nameOe">Имя</label>
+            <input type="text" id="nameOe"
+              ref={this.nameOe}
+              onChange={this.onNameChange} />
+            {!this.state.nameValid &&
+              <div className="error-text">
+                Обязательное поле имени  
+              </div>
+            }
+          </div>
+          <div>
+            <label htmlFor="ageOe">Возраст</label>
+            <input type="number" id="ageOe"
+              ref={this.ageOe}
+              onChange={this.onAgeChange} />
+            {!this.state.ageValid &&
+              <div className="error-text">
+                Обязательное поле возраста
+              </div>
+            }
+          </div>
+          <div>
+            <label htmlFor="roleOe">Роль</label>
+            <select id="roleOe"
+              ref={this.roleOe}
+              onChange={this.onRoleChange}>
+              {this.state.roles.map((r, index) =>
+                <option key={index} value={r}>
+                  {r}
+                </option>
+              )}
+            </select>
+            {!this.state.roleValid &&
+              <div className="error-text">
+                Обязательное поле роли
+              </div>
+            }
+          </div>
+          <button type="submit">Отправить</button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Pitch;
